e3a950bffc75b3b63b9d317f78d66aeb4e3cb50c
Daniel <dochoahenao@gmail.com>
2022-01-19 22:49:53 -0500

    Cleaning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import CandidateCard from './components/CandidateCard/CandidateCard';
 import VotePlat from './components/VotePlat';
 // Import context
 import { VoteProvider } from './context/voteContext';
